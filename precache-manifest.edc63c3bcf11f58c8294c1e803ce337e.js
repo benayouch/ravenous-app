a923b531e80cc515a8b047bedc673c464c574599
benayouch <anouar.ayouch@gmail.com>
2020-07-22 20:55:18 +0100

    Updates

---
+++ precache-manifest.edc63c3bcf11f58c8294c1e803ce337e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9437d9e0c73503758ee0384144d4494",
+    "url": "/ravenous-app/index.html"
+  },
+  {
+    "revision": "e4d0b65d2e7c7a39d02c",
+    "url": "/ravenous-app/static/css/main.5f6d7535.chunk.css"
+  },
+  {
+    "revision": "9925d90d1471d1ce66f7",
+    "url": "/ravenous-app/static/js/2.99d41395.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ravenous-app/static/js/2.99d41395.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e4d0b65d2e7c7a39d02c",
+    "url": "/ravenous-app/static/js/main.2e6e47f8.chunk.js"
+  },
+  {
+    "revision": "240a1be5be1494aa104e",
+    "url": "/ravenous-app/static/js/runtime-main.e53c47dc.js"
+  },
+  {
+    "revision": "902df4c501596c42571749648e081c71",
+    "url": "/ravenous-app/static/media/background_search_desktop.902df4c5.jpg"
+  },
+  {
+    "revision": "5d0905e66c87b168792e1cf920c613ee",
+    "url": "/ravenous-app/static/media/background_search_mobile.5d0905e6.jpg"
+  }
+]);
